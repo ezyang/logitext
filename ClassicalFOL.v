acaa87e3e5436f2e74b5938ccab3a1f147c937cc
Edward Z. Yang <ezyang@mit.edu>
2012-02-24 18:13:13 -0500

    Some pretty ridiculous tactic-work.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- ClassicalFOL.v
+++ ClassicalFOL.v
@@ -1,14 +1,6 @@
 Require Import List.
 Require Import Classical.
 
-(* a non-empty universe; we shall not concern ourselves with matters of denotation *)
-Parameter U : Set.
-
-Parameter a b c : U.
-Parameter f : U -> U.
-Parameter P : U -> Prop.
-Parameter A B C : Prop.
-
 (* a convenient representation for interpreting sequents; this is how we'll receive inputs
    from the external system. Converting backwards is nontrivial, so we'll just parse
    it out. *)
@@ -26,44 +18,131 @@ Definition denote (s : deduction) : Prop :=
 from getting confused. But this representation will be kind of annoying
 to manipulate *)
 
-(* utilities for manipulating sequents. We take the convention that
-all names generated must have the name Hyp in them, although we
-might be able to relax this convention because our contexts will
-have "no pollution" *)
-Ltac myfresh := fresh "Hyp".
-Ltac introf := let h := myfresh in intro h.
-Ltac explode H :=
-  repeat (let h := myfresh in destruct H as [ h H ]).
-Ltac unwrap := cbv; intro H; explode H.
+Lemma contrapositive : forall (A B : Prop), (~ B -> ~ A) <-> (A -> B).
+  intros; destruct (classic B); tauto.
+Qed.
+
+(* Slightly opaque definition to help us keep track of hypotheses
+versus parametric values *)
+Definition Hyp (x : Prop) := x.
+
+Ltac conjExplode H :=
+  repeat (let h := fresh in destruct H as [ h H ]; let x := type of h in change (Hyp x) in h); destruct H.
+Ltac negDisjExplode H :=
+  repeat (let h := fresh in apply not_or_and in H; destruct H as [ h H ]; let x := type of h in change (Hyp x) in h); clear H. (* clear, because destruct does the wrong thing *)
+Ltac unwrap := cbv; intro H; conjExplode H.
 
 (* sequent calculus rules *)
 
-Ltac dCharge H := exact H.
+Ltac myExact := fail.
+Ltac myCut := fail.
+
+Ltac negImp H :=
+  match type of H with Hyp (~ (_ (* H1 *) -> _ (* H2 *))) =>
+    unfold Hyp in H; apply imply_to_and in H;
+    (* H : H1 /\ ~ H2 *)
+    let H1 := fresh in
+    let H2 := fresh in
+    destruct H as [H1 H2];
+    (* Want to move H1 to the bottom (as a negative); keep H2 up top *)
+    match goal with
+      [ |- ?G ] =>
+        let h := fresh in
+        let T1 := type of H1 in
+        assert (h : ~ T1 \/ G); [|
+          let nH1 := fresh in let G' := fresh in
+          destruct h as [ nH1 | G' ]; [ contradict H1; exact nH1 | exact G' ]
+        ]
+    end;
+    clear H1;
+    let T2 := type of H2 in change (Hyp T2) in H2;
+    apply not_and_or
+  end.
+
+Ltac rollupPositive H :=
+  repeat match goal with
+           | [ H' : Hyp ?P |- _ ] => revert H' end;
+  assert (H : True) by trivial;
+  repeat match goal with
+           | [ |- _ -> _ ] =>
+             let h := fresh in intro h;
+             let ht := type of h in
+             let Ht := type of H in
+             let g := fresh in
+             assert (ht /\ Ht) as g; [constructor; assumption |];
+             clear H; clear h; rename g into H
+         end;
+  unfold Hyp in H;
+  revert H.
+
+Ltac posneg :=
+  let H := fresh in
+  rollupPositive H;
+  apply contrapositive;
+  intro H;
+  negDisjExplode H.
+
+(* refactor this with rollupPositive *)
+Ltac rollupNegative H :=
+  repeat match goal with
+           | [ H' : Hyp ?P |- _ ] => revert H' end;
+  assert (H : ~ False) by tauto;
+  repeat match goal with
+           | [ |- _ -> _ ] =>
+             let h := fresh in intro h;
+             let ht := type of h in
+             let Ht := type of H in
+             let g := fresh in
+             assert (ht /\ Ht) as g; [constructor; assumption |];
+             clear H; clear h; rename g into H;
+             apply and_not_or in H
+         end; revert H.
+
+Ltac negpos :=
+  let H := fresh in
+  rollupNegative H;
+  apply <- contrapositive;
+  intro H;
+  conjExplode H.
+
+Ltac canonicalize := posneg; negpos.
 
 (* alternatively, duplicate the hypothesis and only provide fst and snd projection *)
-Ltac lConj H := let h1 := myfresh in let h2 := myfresh in destruct H as [ h1 h2 ].
-Ltac lDisj H := destruct H as [ H | H ].
-(*Ltac lImp H := match type of H with ?P -> ?Q => assert P; [|clear H] end.*)
-Ltac lBot H := destruct H.
-(*Ltac lForall H t := specialize (H t).
-Ltac lExists H := destruct H.*)
+Ltac lConj H :=
+  match type of H with Hyp (_ /\ _) =>
+    let h1 := fresh in let h2 := fresh in unfold Hyp in H; destruct H as [h1 h2];
+    let t1 := type of h1 in let t2 := type of h2 in change (Hyp t1) in h1; change (Hyp t2) in h2
+  end; canonicalize.
+Ltac lDisj H := fail.
+Ltac lImp H := fail.
+(*
+  match type of H with Hyp (?P -> ?Q) =>
+    match goal with [ |- ?G ] =>
+      assert P; *)
+Ltac lBot H := fail.
+Ltac lForall H t := fail.
+Ltac lExists H := fail.
+Ltac lDup H := fail.
 
-(*Ltac rConj := constructor.
-Ltac rDisjL := left.
-Ltac rDisjR := right.
-Ltac rImp := introf.
-Ltac rForall := intro. (* fix me *)
-Ltac rExists t := exists t.*)
+Ltac rConj := fail.
+Ltac rDisjL := fail.
+Ltac rDisjR := fail.
+Ltac rImp H := posneg; negImp H; negpos.
+Ltac rForall := fail.
+Ltac rExists t := fail.
+Ltac rDup := fail.
 
-Ltac dCut H := (* cut H; [introf|] // wrong order *)
-  let h := myfresh in assert (h : H).
+Section universe.
 
-(* convenience *)
-Ltac dup H := let x := type of H in dCut x; [exact H|].
+Variable U : Set.
+Variable z : U. (* non-empty domain *)
+Variables A B C : Prop. (* some convenient things to instantiate with *)
 
 (* an example *)
-Goal denote (nil |= [ ((A -> B) -> A) -> A ] ).
+Goal denote ( [ True; C; C /\ C ] |= [ ((A -> B) -> A) -> A; False ] ).
   unwrap.
-  
-  
-Qed.
+  lConj H2.
+  rImp H0.
+Abort.
+
+End Section.
