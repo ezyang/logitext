5b9fbbd940f27df4da48dac760ccde8b35eab311
Edward Z. Yang <ezyang@mit.edu>
2012-03-10 09:16:06 -0800

    Make the FFI connective tissue.
    Signed-off-by: Edward Z. Yang <ezyang@mit.edu>

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
-#!/bin/sh
+#!/bin/bash
 # Continuous build script
 while true
 do
+    pkill logitext.exe
     reset
+    gcc -c coq.c
     urweb logitext
     ./logitext.exe &
-    inotifywait -e modify *.ur *.urs *.urp *.h
-    pkill logitext.exe
+    inotifywait -e modify $(git ls-files)
 done
